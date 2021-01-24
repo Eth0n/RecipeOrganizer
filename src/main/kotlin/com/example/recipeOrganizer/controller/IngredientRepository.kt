976eb4618cfdf13fa2797b1aa09202246de4b84b
Max <maggshofmann@aol.com>
2021-01-24 09:52:13 +0100

    Fix IngredientRepository

--- src/main/kotlin/com/example/recipeOrganizer/controller/IngredientRepository.kt
+++ src/main/kotlin/com/example/recipeOrganizer/controller/IngredientRepository.kt
@@ -1,11 +1,12 @@
 package com.example.recipeOrganizer.controller
 
 import com.example.recipeOrganizer.entity.Ingredient
+import com.example.recipeOrganizer.entity.Recipe
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 @RepositoryRestResource(collectionResourceRel = "ingredients", path = "ingredients")
 interface IngredientRepository: PagingAndSortingRepository<Ingredient, Long> {
-    fun findByReceipt(@Param("recipe") receipt : Long): List<Ingredient>
+    fun findByRecipe(@Param("recipe") recipe : Long): List<Ingredient>
 }
