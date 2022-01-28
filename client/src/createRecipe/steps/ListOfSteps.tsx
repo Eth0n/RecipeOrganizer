960d5897b202f95cc2f75d1d9c0e97e770711d99
Mandy <dev@mandyr.de>
2022-01-28 07:27:04 +0100

    Remove UsedIngriedientBuilder

--- client/src/createRecipe/steps/ListOfSteps.tsx
+++ client/src/createRecipe/steps/ListOfSteps.tsx
@@ -41,22 +41,3 @@ export function ListOfSteps(props: ListOfStepsProps) {
         </>
     );
 }
-
-// export function getMockStep(desc: string): Step {
-//     const ing1 = getMockIngredient("Zwiebeln");
-//     const ing2 = getMockIngredient("Tomate");
-//     const step = new StepBuilder()
-//         .description(desc)
-//         .addIngredient(ing1)
-//         .addIngredient(ing2)
-//         .build();
-//     return step;
-// }
-
-// function getMockIngredient(name: string): UsedIngredient {
-//     return new UsedIngredientBuilder()
-//         .name(name)
-//         .quantity(10)
-//         .unit({ name: "Gramm", shortDescription: "gr" })
-//         .build();
-// }
